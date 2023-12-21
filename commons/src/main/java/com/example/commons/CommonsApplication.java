f2e917874bab6239e8653d8cd639a7ff84ed58a3
rollo <rollovitch.roll@yandex.ru>
2023-12-21 20:02:23 +0300

    + JWT

---
+++ commons/src/main/java/com/example/commons/CommonsApplication.java
@@ -0,0 +1,9 @@
+package com.example.commons;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+public class CommonsApplication {
+}
